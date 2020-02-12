5b2dabf3c74509f0cad5db4fc70fbaf6d9215c78
Zami <scs.zami@gmail.com>
2020-02-13 02:41:18 +0600

    changes

--- plot4.R
+++ plot4.R
@@ -4,7 +4,7 @@ with(power_consumption_data, {
     plot(Global_active_power~DateTime, type="l", 
          ylab="Global Active Power (kilowatts)", xlab="")
     plot(Voltage~DateTime, type="l", 
-         ylab="Voltage (volt)", xlab="")
+         ylab="Voltage (volt)", xlab="datetime")
     plot(Sub_metering_1~DateTime, type="l", 
          ylab="Global Active Power (kilowatts)", xlab="")
     lines(Sub_metering_2~DateTime,col='Red')
@@ -12,6 +12,6 @@ with(power_consumption_data, {
     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
            legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
     plot(Global_reactive_power~DateTime, type="l", 
-         ylab="Global Rective Power (kilowatts)",xlab="")
+         ylab="Global Rective Power (kilowatts)",xlab="datetime")
 })
 dev.off()
